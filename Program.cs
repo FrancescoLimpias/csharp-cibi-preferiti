5c43929a9b0dc2e6917142aae7f4036e66f5cfec
Francesco Limpias <francescolimpias@gmail.com>
2023-04-04 14:23:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+String[] favoriteDishes =
+{
+    "Pizza",
+    "Poké",
+    "Biscuits",
+    "Fruit Tart",
+    "Spicy chicken wings",
+    "Chocolate",
+    "Ice Cream",
+};
