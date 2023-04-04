9b76dff594115033d0a3f094fa33a4d561a10b20
Francesco Limpias <francescolimpias@gmail.com>
2023-04-04 15:37:37 +0200

    Improved printing

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//Header
 Console.WriteLine("Welcome!");
 Console.WriteLine();
 
+//Array Declaration
 String[] favoriteDishes =
 {
     "Pizza",
@@ -10,26 +13,72 @@ String[] favoriteDishes =
     "Fruit Tart",
     "Spicy chicken wings",
     "Chocolate",
-    "Ice Cream",
+    //"Ice Cream",
 };
 
+//Main
 Console.WriteLine($"These are my {favoriteDishes.Length} favorite dishes:");
 for (int i = 0; i < favoriteDishes.Length; i++)
 {
-    Console.Write($" {i + 1}° - {favoriteDishes[i]}");
 
+    //Ranking assignment
+    Console.Write($" {i + 1}° ");
+
+    //Key role assignment
+    string role = "NONE";
     if (i == 0)
-        Console.Write(" (MY FAVORITE!)");
+        role = "TOP";
     else if (i == favoriteDishes.Length - 1)
-        Console.Write(" (THE'LESS FAVORITE')");
+        role = "BOTTOM";
     else if (
         favoriteDishes.Length % 2 == 0 /* oddity check*/
         && i + 1 == favoriteDishes.Length / 2 /* even */
         || i == favoriteDishes.Length / 2) /* odd and even */
-        Console.Write(" - HALF");
+        role = "MID";
+
+    //Bullet print
+    switch (role)
+    {
+        case "TOP":
+            Console.Write("▲");
+            break;
+        case "BOTTOM":
+            Console.Write("▼");
+            break;
+        case "MID":
+            Console.Write("-");
+            break;
+        default:
+            Console.Write("|");
+            break;
+    }
+
+    //Food print
+    Console.Write($" {favoriteDishes[i]}");
+
+    //Added TOP & BOTTOM highlight
+    if(role == "TOP")
+    {
+        Console.Write(" (THE FAVORITE!)");
+    } else if(role == "BOTTOM"){
+        Console.Write(" (THE LEAST FAVORITE)");
+    }
 
     Console.WriteLine();
 }
 Console.WriteLine();
 
+//Footer
 Console.WriteLine("Thank you!");
+
+
+
+//if (i == 0)
+//    Console.Write(" (MY FAVORITE!)");
+//else if (i == favoriteDishes.Length - 1)
+//    Console.Write(" (THE'LESS FAVORITE')");
+//else if (
+//    favoriteDishes.Length % 2 == 0 /* oddity check*/
+//    && i + 1 == favoriteDishes.Length / 2 /* even */
+//    || i == favoriteDishes.Length / 2) /* odd and even */
+//    Console.Write(" - HALF");
